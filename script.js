40152fc586460d440255d845311145abfef56e91
Jakob Lingel <jlingel68@gmail.com>
2022-05-04 12:37:58 +0200

    Add win logic

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const isWin = () => {
-	if (mark.x1 == mark.x2 == mark.x3) return win;
-	if (mark.y1 == mark.y2 == mark.y3) return win;
-	if ((mark.x1 != mark.x2 != mark.x3)
-		&& (mark.y1 != mark.y2 != mark.y3)) return win;
+
+const player = () => {
+	let currentPlayer;
+	const placeMarker = (x, y) => {
+	}
 }
 
 const board = (() => {
-
 	const boardArray = (function(boardSize) {
 		const point = (x, y) => {
-			return { x, y, "set": null};
+			return { x, y, "set": null };
 		}
 		let boardArray = [];
 		let counter = 0;
@@ -22,29 +21,89 @@ const board = (() => {
 		return boardArray;
 	})(3);
 
-	console.log(boardArray[2]);
 	const checkOccupied = (index) => (boardArray[index].set === null) ? false : true;
-	const setOccupied = (index, player) => {
+	const setOccupied = (index, player, field) => {
 		if (checkOccupied(index)) return false;
 		boardArray[index].set = player;
+		return true;
 	}
-	setOccupied(2, "player1");
-	setOccupied(2, "player2");
-	console.log(boardArray[2]);
+
+	const htmlFields = ((boardArray) => {
+		let fieldArray = [];
+		for (let i = 0; i < boardArray.length; i++) {
+			let field = document.createElement("div");
+			field.id = `field_${i}`;
+			field.classList.add("field");
+			fieldArray.push(field);
+		}
+		return {
+			fieldArray
+		}
+	})(boardArray);
+
+	const renderHtmlBoard = () => {
+		const boardElement = document.querySelector(".board");
+		console.log(htmlFields);
+		htmlFields.fieldArray.forEach(elem => {
+			console.log(elem);
+			boardElement.appendChild(elem);
+		});
+	}
+
 	return {
-		checkOccupied,
+		setOccupied,
+		htmlFields,
+		renderHtmlBoard,
+		boardArray
 	}
 })();
 
-const player = () => {
-	const placeMarker = (x, y) => {
+const game = () => {
+	const counterFactory = (() => {
+		let counter = 0;
+		return function() {
+			counter++
+			console.log(counter);
+			return counter;
+		}
+	})();
+	const startGame = () => {
 	}
+	let currentPlayer = "player1";
+	const switchTurn = (currentPlayer) => {
+		return (currentPlayer == "player1" ? "player2" : "player1");
+	}
+	const getPlayer = () => currentPlayer;
+
+	let counter = counterFactory;
+	board.htmlFields.fieldArray.forEach(elem => {
+		elem.addEventListener('click', () => {
+			let count = counter();
+			if (!board.setOccupied(elem.id.slice(-1), currentPlayer)) return false;
+			elem.classList.add(`marker-${getPlayer()}`);
+			if (isWin(currentPlayer)) {
+				console.log(`${currentPlayer} won`);
+			}
+			currentPlayer = switchTurn(getPlayer());
+		});
+	});
+	const isWin = (player) => {
+		let marksPlayer = board.boardArray.filter(elem => {
+			return elem.set == player;
+		});
+		if (marksPlayer.length >= 3) {
+			console.log(marksPlayer);
+			for (let i = 0; i < marksPlayer.length; i++) {
+				console.log(marksPlayer.filter(elem => elem.x != marksPlayer[i].x));
+				return (marksPlayer.filter(elem => elem.x == marksPlayer[i].x).length == 3)
+					|| (marksPlayer.filter(elem => elem.y == marksPlayer[i].y).length == 3)
+					|| ((marksPlayer.find(elem => elem.x == 1 && elem.y == 1) && (marksPlayer.filter(elem => elem.x != marksPlayer[i].x).length >= 2)
+						&& (marksPlayer.filter(elem => elem.y != marksPlayer[i].y)).length >= 2))
+			}
+		}
+	}
+	return Object.assign({}, { board }, { switchTurn }, { startGame }, { getPlayer })
 }
 
-const game = () => {
-	const switchTurn = (currentPlayer) => {
-	}
-	return {
-		switchTurn
-	}
-}
+let myGame = game();
+myGame.board.renderHtmlBoard();
